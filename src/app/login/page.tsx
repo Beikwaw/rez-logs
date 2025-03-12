51c0425e0c6826446388dc25f0620795352585c5
Aliyu Yahaya <st10440112@vcconnect.edu.za>
2025-03-13 00:53:12 +0200

    f4

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React, { useEffect, useState } from 'react';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation'; // Updated import
 
 interface LoginPageProps {
     userType: 'student' | 'admin';
